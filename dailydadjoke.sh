f55170b874bf1afd0d7e361ad608bb0a76f4f190
Blaize <me@blaize.net>
2021-07-28 14:46:42 -0400

    fix json format

--- dailydadjoke.sh
+++ dailydadjoke.sh
@@ -9,10 +9,12 @@ let DIFF=$(( (`date -d "00:00" +%s` - `date -d $startDate +%s`) / (24*3600) ))
 x=0
 while read p; do
 	x=$((x+1))
-	if [ $x = $DIFF ];then
-		echo "$p"
+	if [ $x = $DIFF ];then		
 		p=${p//[$'\t\r\n']}
-		echo $(cat ./docs/jokehistory.js | jq --arg jk "$p" '.history += [$jk]') > ./docs/jokehistory.js
+		d=$(date -d "00:00" -u +"%Y-%m-%dT%H:%M:%SZ")
+		json=$(jq --arg d "$d" --arg t "$p" '. + {date:$d} + {text:$t}'<<<"{}")
+		echo $json
+		echo $(cat ./docs/jokehistory.js | jq --argjson jk "$json" '.history += [$jk]') > ./docs/jokehistory.js
 	fi
 	
 done < general.txt
--- docs/jokehistory.js
+++ docs/jokehistory.js
@@ -1,1 +1,1 @@
-{ "history": [ "What did the ocean say to the beach? Nothing, it just waved." ], "hashTags": "" }
+{ "history": [], "hashTags": "" }
