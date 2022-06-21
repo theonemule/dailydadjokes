9c02f0beb41d18c57654ecda1171a59565068f70
Blaize Stewart <blaize.stewart@atmosera.com>
2022-06-21 15:14:18 -0400

    to js

--- twitter.js
+++ twitter.js
@@ -1,55 +1,33 @@
 const Twitter = require('twitter');
 const fs = require('fs');
-const request = require('request');
 
+
+var startDate = new Date(2021,6,7);//startDate="20210707"//
+var diff = Math.floor((new Date().getTime() - startDate.getTime()) / (1000 * 3600 * 24));
+console.log(diff)
+
+const allFileContents = fs.readFileSync('general.txt', 'utf-8');
+var jokes = allFileContents.split(/\r?\n/)
+var joke = jokes[diff]
+
+var tweettxt = `${joke}
+
+http://www.dailydadjokes.net
+
+#humor #dadjoke #dailydadjoke`
 
 var client = new Twitter({
-  consumer_key: process.env.TW_CONSUMER_KEY,
-  consumer_secret: process.env.TW_CONSUMER_SECRET,
-  access_token_key: process.env.TW_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TW_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
-var category = "";
-
-if(process.argv.length > 2){
-	category = process.argv[2]
-}
-
-
-const data = fs.readFileSync('./tweets.json',
-            {encoding:'utf8', flag:'r'});
-
-var records = JSON.parse(data)
-
-if (records.length > 0){
-	var queuedTweet;
-	
-	if(category == ""){
-		queuedTweet = records.splice(0,1)[0];
-	}else{
-		for(var i = 0; i < records.length; i++){
-			if(records[i].category == category){
-				queuedTweet = records.splice(i,1)[0];
-				break;
-			}
-		}
-	}
-	
-
-	if(queuedTweet){
-		
-		if(queuedTweet.evergreen){
-			records.push(queuedTweet);
-		}		
-		
-		client.post('statuses/update', {status: queuedTweet.tweet})
-			.then(function (tweet) {
-				console.log(tweet);
-				fs.writeFileSync("./tweets.json", JSON.stringify(records, null, 2));							
-			})
-			.catch(function (error) {
-				console.log(error);
-			});	
-	}
-}
+client.post('statuses/update', {status: tweettxt})
+	.then(function (tweet) {
+		console.log(tweet);
+		fs.writeFileSync("./tweets.json", JSON.stringify(records, null, 2));							
+	})
+	.catch(function (error) {
+		console.log(error);
+	});	
