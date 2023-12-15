aa580cb3b9a02ddeec55849956a0941ae8843428
Blaize <me@blaize.net>
2023-12-15 15:20:10 -0500

    Update dailydadjoke.sh

--- dailydadjoke.sh
+++ dailydadjoke.sh
@@ -2,7 +2,8 @@
 
 response=$(curl -s "https://func-dadjokesaieastus2.azurewebsites.net/api/DadJokesAI_Functions?code=$DADJOKECODE")
 
-p=${p//[$'\t\r\n']}
+# p=${p//[$'\t\r\n']}
+
 d1=$(date -d "00:00" -u +"%Y-%m-%dT%H:%M:%SZ")		
 d2=$(date -d "00:00" -u +"%Y-%m-%d")
 fname=$(echo "$d2-joke.md")
@@ -11,4 +12,4 @@ echo "layout: post" >> ./_posts/$fname
 echo "title:  Daily Dad Joke 4U" >> ./_posts/$fname
 echo "date:   $d1" >> ./_posts/$fname
 echo "---" >> ./_posts/$fname
-echo "$p" >> ./_posts/$fname
+echo "$response" >> ./_posts/$fname
